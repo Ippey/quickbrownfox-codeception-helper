aa4fee41059058c0cb00f6b24c5a52edc4993c66
Ippei Sumida <ippey.s@gmail.com>
2018-06-28 10:20:32 +0900

    Add unit test. #2

---
+++ tests/unit/QuickBrownFoxTest.php
@@ -0,0 +1,42 @@
+<?php
+
+class QuickBrownFoxTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    /** @var \Helper\QuickBrownFox */
+    private $helper;
+
+    /**
+     * @throws Exception
+     */
+    protected function _before()
+    {
+        $config = [
+            'dsn' => 'sqlite:tests/_data/test.db',
+            'user' => 'testuser',
+            'password' => 'testpass',
+        ];
+        /** @var \Codeception\Lib\ModuleContainer $moduleContainer */
+        $moduleContainer = \Codeception\Stub::make(\Codeception\Lib\ModuleContainer::class);
+        $this->helper = new \Helper\QuickBrownFox($moduleContainer, $config);
+        $method = new ReflectionMethod(\Helper\QuickBrownFox::class, 'createConnection');
+        $method->setAccessible(true);
+        $method->invoke($this->helper);
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testNewFixtureSession()
+    {
+        $this->helper->newFixtureSession();
+        $session = $this->helper->getFixtureSession();
+        $this->assertInstanceOf(\Lapaz\QuickBrownFox\Database\FixtureSetupSession::class, $session);
+    }
+}
